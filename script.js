19ea93a1a4d01e81ded37a13244dfc5266b9fc69
Carlos Minarelli <cminarelli@korewireless.com>
2022-07-31 15:19:08 -0300

    Interacao do menu

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+function openMenu(){
+    document.getElementById("nav").onmouseleave= function() {closeMenu()}
+    document.getElementById("header").style.width = "20%"
+    document.getElementById("aside").style.visibility = "visible"
+    document.getElementById("aside").style.opacity = "1"
+    
+    setTimeout(() => {
+        document.getElementById("menu").style.visibility = "visible"
+        document.getElementById("menu").style.opacity ="1"
+    }, 200)
+}
+
+function closeMenu() {
+    document.getElementById("menu").style.visibility = "hidden"
+    document.getElementById("menu").style.opacity ="0"
+    document.getElementById("aside").style.visibility = "hidden"
+    document.getElementById("aside").style.opacity = "0"
+    setTimeout(() => {
+        document.getElementById("header").style.width = "5%"
+    }, 150)
+}
