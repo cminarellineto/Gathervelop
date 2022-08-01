2aa00fe25ee4e8e52023cac36a53a26e00b0f85e
cminarellineto <cminarellineto@gmail.com>
2022-08-01 13:44:30 -0300

    pequenas alterações

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function openMenu(){
     document.getElementById("header").style.width = "20%"
     document.getElementById("aside").style.visibility = "visible"
     document.getElementById("aside").style.opacity = "1"
+    document.getElementById("menuIcon").style.display = "none"
     
     setTimeout(() => {
         document.getElementById("menu").style.visibility = "visible"
@@ -17,6 +18,7 @@ function closeMenu() {
     document.getElementById("aside").style.visibility = "hidden"
     document.getElementById("aside").style.opacity = "0"
     setTimeout(() => {
+        document.getElementById("menuIcon").style.display = "flex"
         document.getElementById("header").style.width = "5%"
     }, 150)
 }
